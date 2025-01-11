6ad02a5723254d0cd36aef2662a4b13bd5d5c635
AadarshGupta04 <aadarshgupta56789@gmail.com>
2025-01-11 17:06:48 +0530

    New Commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import DealershipPage from "./DealershipPage";
 
 function App() {
   return (
-    <></>
+    <Router>
+      <Switch>
+        {/* Set DealershipPage as the default route */}
+        <Route exact path="/" component={DealershipPage} />
+        {/* Add other routes here if needed */}
+      </Switch>
+    </Router>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import Navbar from './Navbar';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -12,6 +11,10 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
     <Navbar />
-    <DealershipPage></DealershipPage>
+    <Routes>
+      {/* Define the main route for the DealershipPage */}
+      <Route path="/" element={<DealershipPage />} />
+      {/* You can add more routes here if needed */}
+    </Routes>
   </BrowserRouter>
 );
